a0c2c2f4299bb16cd34bac9dcea45fee03c25819
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-07-08 20:57:55 +0100

    Add logic for interactiveness to clear and edit todo task

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import './index.css';
+import refreshPage from './refresh.js';
 
 const addText = document.querySelector('#add-text');
 let todos = JSON.parse(localStorage.getItem('todo-list'));
 const todoTask = document.querySelector('.todo-task');
+const clearTask = document.querySelector('.clear-task');
+
+refreshPage();
+
+const removeCompletedFromUI = () => {
+  const allCompleted = document.querySelectorAll('.checkedColor');
+  allCompleted.forEach((completed) => {
+    completed.remove();
+  });
+};
+
+const removeCompletedFromStorage = () => {
+  const todosLeft = todos.filter((todo) => (todo.isCompleted === false));
+  todosLeft.forEach((td, index) => {
+    td.idx = index;
+  });
+  localStorage.setItem('todo-list', JSON.stringify(todosLeft));
+};
 
 const displayTodo = () => {
   let li = '';
@@ -12,7 +31,7 @@ const displayTodo = () => {
       <div class = todo-dynamic>
       <label for id = "${id}" class="todo-input">
           <input onclick = 'update(this)' class = 'check-task' type = "checkbox" id = "${id}">
-          <p class = "todo-text">${todo.description} </p>
+          <p class = "todo-text">${todo.description}</p>
       </label>
       <div class= "settings">
          <i class = "fa fa-ellipsis-v"></i>
@@ -33,6 +52,7 @@ const deleteTask = (deleteId) => {
   todos.splice(deleteId, 1);
   displayTodo();
   localStorage.setItem('todo-list', JSON.stringify(todos));
+  window.location.reload();
 };
 
 window.deleteTask = deleteTask;
@@ -59,35 +79,68 @@ const update = (selectedTask) => {
 
 window.update = update;
 
-addText.addEventListener('keyup', (e) => {
-  const userTask = e.target.value.trim();
-  if (e.key === 'Enter' && userTask) {
-    if (!todos) {
-      todos = [];
+const addT = () => {
+  addText.addEventListener('keyup', (e) => {
+    const userTask = e.target.value.trim();
+    if (e.key === 'Enter' && userTask) {
+      if (!todos) {
+        todos = [];
+      }
+      addText.value = '';
+      const taskInfo = {
+        description: userTask,
+        isCompleted: false,
+        idx: todos.length,
+      };
+      todos.push(taskInfo);
+      localStorage.setItem('todo-list', JSON.stringify(todos));
+      displayTodo();
+      window.location.reload();
     }
-    addText.value = '';
-    const taskInfo = {
-      description: userTask,
-      isCompleted: false,
-      idx: todos.length,
-    };
-    todos.push(taskInfo);
-    localStorage.setItem('todo-list', JSON.stringify(todos));
-    displayTodo();
-  }
-});
+  });
+};
+
+addT();
+
+const saveEditTextToStorage = (task, newTask) => {
+  const tasks = JSON.parse(localStorage.getItem('todo-list'));
+  tasks.forEach((t) => {
+    if (task === t.description) {
+      t.description = newTask;
+    }
+    localStorage.setItem('todo-list', JSON.stringify(tasks));
+  });
+};
 
 const editList = () => {
   const allThreeDotBtn = document.querySelectorAll('.fa-ellipsis-v');
   allThreeDotBtn.forEach((threeDotBtn) => {
-    threeDotBtn.addEventListener('click', () => {
+    threeDotBtn.addEventListener('click', (e) => {
+      e.preventDefault();
       const taskEdit = threeDotBtn.parentElement.parentElement.firstElementChild.lastElementChild;
+      const oldTask = taskEdit.textContent;
       taskEdit.toggleAttribute('contentEditable');
       taskEdit.focus();
       threeDotBtn.parentElement.parentElement.classList.toggle('checkedColor');
-      localStorage.setItem('todo-list', JSON.stringify(todos));
+      taskEdit.addEventListener('keypress', (e) => {
+        if (e.key === 'Enter') {
+          taskEdit.removeAttribute('contentEditable');
+          threeDotBtn.parentElement.parentElement.classList.remove('checkedColor');
+          const newTask = taskEdit.textContent;
+          saveEditTextToStorage(oldTask, newTask);
+        }
+      });
     });
   });
 };
 
 editList();
+
+clearTask.addEventListener('click', () => {
+  todos.forEach((todo) => {
+    if (todo.isCompleted) {
+      removeCompletedFromUI();
+      removeCompletedFromStorage();
+    }
+  });
+});
